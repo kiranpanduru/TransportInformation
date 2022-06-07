f6691a5e8171a257f27f2bd099607fd488a8ef88
kiranpanduru <kiranpandupvmr@gmail.com>
2022-06-07 17:54:20 +0530

    new code

--- src/org/air/Air.java
+++ src/org/air/Air.java
@@ -3,6 +3,7 @@ package org.air;
 public class Air {
 	public void aeroPlane() {
     System.out.println(" aeroplane is AIR INDIA ");
+    System.out.println("aeroplane is INDIAN AIRLINES ");
 	}
 	public void heliCopter() {
     System.out.println(" helicopter is AVENGER");
